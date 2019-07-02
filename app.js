9875e5e89d97963f90c239adfec9f02e155b19ce
Anton Pugachevsky <Anton.Pugachevsky@LibertyMutual.com>
2019-07-02 15:23:07 -0400

    filter red line

--- app.js
+++ app.js
@@ -10,9 +10,7 @@ app.message('knock knock', ({ message, say }) => {
     var eventSourceInitDict = {headers: {'accept': 'text/event-stream', 'x-api-key': '77908788c1bc4fbbacb489f5bc7907cf'}};
     var evtSource = new EventSource("https://api-v3.mbta.com/alerts", eventSourceInitDict);
     evtSource.onmessage = function(e) {
-        if(e.event === 'update') {
-            say(e.data.attributes.header);
-        }
+        say(e.data.attributes.header);
       };
       evtSource.onerror = function (err) {
         console.log(err);
