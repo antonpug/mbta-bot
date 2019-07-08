c791fa15386da310e0539f20dbfd6a6fde08ccc0
Anton Pugachevsky <Anton.Pugachevsky@LibertyMutual.com>
2019-07-08 14:30:35 -0400

    try something

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = new App({
 app.message('knock knock', ({ message, say }) => {
   var eventSourceInitDict = { headers: { 'accept': 'text/event-stream', 'x-api-key': '77908788c1bc4fbbacb489f5bc7907cf' } };
   var evtSource = new EventSource("https://api-v3.mbta.com/predictions/?filter\\[stop\\]=place-sstat&stop_sequence=1", eventSourceInitDict);
-  console.log('registered'); ''
+  console.log('registered', evtSource); ''
   evtSource.addEventListener('message', function (e) {
     console.log(e.data);
   }, false);
